2a725753ded1465e4a6262f917ec4fd4fc1a490c
Yasha <yassine.damiri@epita.fr>
2022-11-19 10:58:33 +0100

    feat(Home): Added home

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,20 +4,20 @@ import { covers } from "../data";
 
 export default function Home() {
     return (
-        <div className="bg-dark text-white flex min-h-screen max-h-screen overflow-y-auto overflow-x-hidden">
+        <div className="flex overflow-y-auto overflow-x-hidden min-h-screen max-h-screen text-white bg-dark">
             <div className="nav bg-red-500 w-[52px] p-5 box-content"></div>
 
-            <div className="app bg-blue-500 grow max-h-screen flex flex-col">
+            <div className="flex flex-col max-h-screen bg-blue-500 app grow">
                 <div className="searchbar bg-green-500 w-full h-[52px] p-5 box-content"></div>
 
-                <div className="content w-full p-5 overflow-auto pb-24">
-                    <div class="flex ">
-                        <div className="bg-red-500 w-48"></div>
+                <div className="overflow-auto p-5 pb-24 w-full content">
+                    <div class="flex">
+                        <div className="w-48 bg-red-500"></div>
                     </div>
                 </div>
             </div>
 
-            <div className="player absolute bottom-0 h-20 w-full bg-yellow-500"></div>
+            <div className="absolute bottom-0 w-full h-20 bg-yellow-500 player"></div>
         </div>
     );
 }
