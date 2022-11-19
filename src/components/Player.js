951a1c48c4c1ced1024c813b064dec20f8ddd80c
Mickael Bobovitch <mickael.bobovitch@epita.fr>
2022-11-19 16:46:16 +0100

    mms

---
+++ src/components/Player.js
@@ -0,0 +1,78 @@
+import { useState } from "react";
+
+export const Player = () => {
+    const [isPlaying, setIsPlaying] = useState(true);
+
+    const togglePlay = () => {
+        setIsPlaying(!isPlaying);
+    };
+
+    return (
+        <div className="w-screen">
+
+            {/* music cover and title */}
+            <div className="flex items-center h-24">
+                <div className="song w-52">
+                    <div className="flex items-center">
+                        <div className="w-24 h-24 p-4">
+                            <img
+                                src="https://i.scdn.co/image/ab67656300005f1f301e998c4927b7d7a6c7b224"
+                                alt="cover"
+                                className="rounded shadow shadow-white"
+                            />
+                        </div>
+                        <div className="mr-4">
+                            <div className="text-sm">Song Name</div>
+                            <div className="text-xs">Artist Name</div>
+                        </div>
+                    </div>
+                </div>
+
+                {/* play/pause button */}
+                <div className="player grow h-full flex flex-col pt-2">
+                    <div className="controls flex">
+                        <div className="mx-auto">
+                            <button className="">
+                                <img
+                                    src="/back.svg"
+                                    alt="player"
+                                    className="fill-orange-500"
+                                />
+                            </button>
+
+                            <button className="mx-4" onClick={togglePlay}>
+                                <img
+                                    src={isPlaying ? "/pause.svg" : "/play.svg"}
+                                    alt="player"
+                                    className="fill-orange-500"
+                                />
+                            </button>
+
+                            <button className="">
+                                <img
+                                    src="/next.svg"
+                                    alt="player"
+                                    className="fill-orange-500"
+                                />
+                            </button>
+                        </div>
+                     </div>
+                    <div className="progress flex">
+                        <div className="m-auto">
+                            <input type="range" className="w-[1700px] max-w-screen-md range" />
+                        </div>
+                    </div>
+                </div>
+
+                {/* volume and repeat */}
+                <div className="volume flex-none w-52 pt-10">
+                    {/* <div className="flex items-center">
+                        <div className="w-24 h-24 bg-red-500"></div>
+                    </div> */}
+                </div>
+            </div>
+
+
+        </div>
+    );
+};
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,11 +4,11 @@ import React from "react";
 export default function SearchBar() {
     return (
         <navbar className="flex-auto w-auto text-light_gray">
-            <div class="relative grow">
-                <div class="flex absolute inset-y-0 left-0 items-center pl-3 text-gray-500 pointer-events-none">
-                    <svg aria-hidden="true" class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+            <div className="relative grow">
+                <div className="flex absolute inset-y-0 left-0 items-center pl-3 text-gray-500 pointer-events-none">
+                    <svg aria-hidden="true" className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
                 </div>
-                <input type="search" id="default-search" class="focus:ring-2 ring-orange-500 focus:outline-[0.5px] border-0 outline-none block p-4 pl-10 text-sm bg-black rounded-lg  w-[1122px] height-[73px] " placeholder="Search" required />
+                <input type="search" id="default-search" className="focus:ring-2 ring-orange-500 focus:outline-[0.5px] border-0 outline-none block p-4 pl-10 text-sm bg-black rounded-lg  w-[1122px] height-[73px] " placeholder="Search" required />
             </div>
         </navbar>
     );
