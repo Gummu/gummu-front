e558c290608b0d03150c933364a9e0d704ea694a
Mickael Bobovitch <mickael.bobovitch@epita.fr>
2022-11-19 10:49:56 +0100

    feat(home page): Added skeleton

---
+++ src/data.js
@@ -0,0 +1,20 @@
+export const covers = [
+    {
+        id: 1,
+        title: "We are the Champions",
+        artist: "Queen",
+        image: "https://e-cdn-images.dzcdn.net/images/cover/1c074bef12457018fd6fb1fcf9098620/264x264-000000-80-0-0.jpg",
+    },
+    {
+        id: 2,
+        title: "Группа крови",
+        artist: "Кино",
+        image: "https://e-cdn-images.dzcdn.net/images/cover/b39e82e1e4fdaf3f08ae582e8c9565cd/264x264-000000-80-0-0.jpg",
+    },
+    {
+        id: 3,
+        title: "Cyberpunk 2077",
+        artist: "Suphi",
+        image: "https://i.scdn.co/image/ab67656300005f1f301e998c4927b7d7a6c7b224",
+    },
+];
