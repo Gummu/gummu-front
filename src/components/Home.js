a89de8ebaf0cc0ffa5e708414948cb4c9e947602
Mickael Bobovitch <mickael.bobovitch@epita.fr>
2022-11-19 10:49:56 +0100

    feat(home page): Added skeleton

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,23 @@
 import React from "react";
+import { Cover } from "./Cover";
+import { covers } from "../data";
 
 export default function Home() {
     return (
-        <div class="m-0 font-sans text-base antialiased font-normal bg-dark">
-            popo
+        <div className="bg-dark text-white flex min-h-screen max-h-screen overflow-y-auto overflow-x-hidden">
+            <div className="nav bg-red-500 w-[52px] p-5 box-content"></div>
+
+            <div className="app bg-blue-500 grow max-h-screen flex flex-col">
+                <div className="searchbar bg-green-500 w-full h-[52px] p-5 box-content"></div>
+
+                <div className="content w-full p-5 overflow-auto pb-24">
+                    <div class="flex ">
+                        <div className="bg-red-500 w-48"></div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="player absolute bottom-0 h-20 w-full bg-yellow-500"></div>
         </div>
     );
 }
